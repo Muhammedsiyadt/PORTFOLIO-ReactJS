03128e175db41d7d24430100fc3c30b9f1cb1160
siyad <siyadkundala10@gmail.com>
2024-07-01 17:34:58 +0530

    color changed

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,19 @@
 const Services_Data = [
     {
-        s_no:"01",
-        s_name:"Web development",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "01",
+        s_name: "Web Development",
+        s_desc: "Building and maintaining websites, focusing on performance and user experience."
     },
     {
-        s_no:"02",
-        s_name:"Web development",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "02",
+        s_name: "Web Designing",
+        s_desc: "Creating visually appealing and user-friendly website designs."
     },
     {
-        s_no:"03",
-        s_name:"Web development",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    // {
-    //     s_no:"04",
-    //     s_name:"App design",
-    //     s_desc:"Web development is the process of building, programming..."
-    // },
-    // {
-    //     s_no:"05",
-    //     s_name:"Digital marketing",
-    //     s_desc:"Web development is the process of building, programming..."
-    // },
-    // {
-    //     s_no:"06",
-    //     s_name:"Content writing",
-    //     s_desc:"Web development is the process of building, programming..."
-    // },
-]
- 
+        s_no: "03",
+        s_name: "UI/UX Design",
+        s_desc: "Designing intuitive interfaces and enhancing user satisfaction."
+    }
+];
+
 export default Services_Data;
